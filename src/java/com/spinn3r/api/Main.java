e4c7ddd13be1b2f77a7e64d856547cd14b59aa6f
Jeff Wu <jwu@spinn3r.com>
2010-11-29 21:17:00 -0800

    add --afterTimestamp and --afterBefore

--- src/java/com/spinn3r/api/Main.java
+++ src/java/com/spinn3r/api/Main.java
@@ -89,7 +89,7 @@ public class Main<T extends BaseResult> {
     /**
      * How far behind should be start by default?
      */
-    public static long INTERVAL = 20L * 60L * 1000L;
+    public static long INTERVAL = Config.millisecondsToTimestamp(20L * 60L * 1000L);
 
     /**
      * The date of the last item we found.
@@ -726,9 +726,9 @@ public class Main<T extends BaseResult> {
             return -1;
 
         try {
-            return Long.parseLong(opt);
+            return Config.millisecondsToTimestamp(Long.parseLong(opt));
         } catch (Throwable t) {
-            return ISO8601DateParser.parse(opt).getTime();
+            return Config.millisecondsToTimestamp(ISO8601DateParser.parse(opt).getTime());
         }
 
     }
@@ -888,10 +888,30 @@ public class Main<T extends BaseResult> {
                 show_results = Integer.parseInt(getOpt(v));
                 continue;
             }
+            
+            /*
+             * The code for the --afterTimestamp must come
+             * before the code for --after because --afterTimestamp
+             * also matches startsWith("after");
+             */
+            if (v.startsWith("--afterTimestamp")) {
+            	after =  Long.parseLong(getOpt(v));
+            	continue;
+            }
 
             if (v.startsWith("--after")) {
                 after = getOptAsTimeInMillis(v);
                 continue;
+            }
+            
+            /*
+             * The code for the --beforeTimestamp must come
+             * before the code for --before because --beforeTimestamp
+             * also matches startsWith("before");
+             */
+            if (v.startsWith("--beforeTimestamp")) {
+            	before = Long.parseLong(getOpt(v));
+            	continue;
             }
 
             if (v.startsWith("--before")) {
@@ -1115,11 +1135,11 @@ public class Main<T extends BaseResult> {
 
         if (after > -1)
             System.out.printf("After: %s (%s)\n", ISO8601DateParser
-                    .toString(new Date(after)), after);
+                    .toString(Config.timestampToDate(after)), after);
 
         if (before > -1)
             System.out.printf("Before: %s (%s)\n", ISO8601DateParser
-                    .toString(new Date(before)), before);
+                    .toString(Config.timestampToDate(before)), before);
 
         System.out.println("Saving results to disk: " + save);
 
@@ -1128,11 +1148,11 @@ public class Main<T extends BaseResult> {
         // getLastRequestURL returned from fetch() below
 
         if (after == -1) {
-            after = System.currentTimeMillis();
+            after = Config.millisecondsToTimestamp(System.currentTimeMillis());
             after = after - INTERVAL;
         }
 
-        config.setAfter(new Date(after));
+        config.setAfterTimestamp(after);
 
         new Main().exec(client, config);
 
