3df44e2e5f8318c1f0e9819bb6181f049831d736
Jeff Wu <jwu@spinn3r.com>
2011-01-07 15:01:18 -0800

    Add get date found field.
    remove javadoc.
    
    bump to 3.3.11

--- src/java/com/spinn3r/api/Config.java
+++ src/java/com/spinn3r/api/Config.java
@@ -55,7 +55,7 @@ public abstract class Config <ResultType> implements Cloneable {
     /**
      * When fetching the API this specifies the default version to return.
      */
-    public static String   DEFAULT_VERSION     = "3.3.09";
+    public static String   DEFAULT_VERSION     = "3.3.11";
 
     public static enum Format {
     	RSS("rss"), PROTOBUF("protobuf"), PROTOSTREAM("protostream");
